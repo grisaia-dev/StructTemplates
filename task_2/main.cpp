6205f8a72ff2a0dce237c3d3001775afcc632b76
Grisaia <grisaia@inbox.ru>
2023-09-11 16:05:31 +0900

    Second task completed

---
+++ task_2/main.cpp
@@ -0,0 +1,18 @@
+#include "lib/proxy.hpp"
+#include <iostream>
+
+int main(void) {
+    // auto real_db = VeryHeavyDatabase();
+    // auto cached_db = CacheProxyDB(std::addressof(real_db));
+    // auto test_db = TestDB(std::addressof(real_db));
+    // std::cout << cached_db.GetData("key") << std::endl;
+    // std::cout << cached_db.GetData("key") << std::endl;
+    // std::cout << test_db.GetData("key") << std::endl;
+
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    return EXIT_SUCCESS;
+}
